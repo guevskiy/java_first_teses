c63550908183f356619b00eeb8f313dc7d2874ef
Artem Guevskiy <Artem Guevskiy@DESKTOP-6MA6TRH>
2018-10-04 12:13:50 +0300

    add tests

--- addressbook-web-tests/src/test/java/com/gmail/guevskiyart/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/com/gmail/guevskiyart/addressbook/tests/GroupCreationTests.java
@@ -7,7 +7,19 @@ import org.testng.annotations.Test;
 public class GroupCreationTests extends TestBase {
 
     @Test
-    public void testGroupCreation(){
+    public void testGroupCreation_01(){
+        app.getNavigationHelper().gotoGroupPage();
+
+        int nn = app.getGroupHelper().getCountOfExistingGroups();
+
+        app.getGroupHelper().initGroupCreation();
+        app.getGroupHelper().fillGroupForm(nn, new GroupData("group_name", "header", "footer"));
+        app.getGroupHelper().submitGroupCreation();
+        app.getGroupHelper().returnToGroupPage();
+    }
+
+    @Test
+    public void testGroupCreation_02(){
         app.getNavigationHelper().gotoGroupPage();
 
         int nn = app.getGroupHelper().getCountOfExistingGroups();
